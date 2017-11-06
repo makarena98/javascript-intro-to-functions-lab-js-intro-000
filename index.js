f843b863a030d8f80ab04efe31f728491093633b
Karen Allen <starting_today@yahoo.com>
2017-11-06 03:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string.toLowerCase()){
       return "I can't hear you!"
+    } else if {
+      (string.toUpperCase()){
+      return "YES INDEED!"}    
     }
 }
 
-function sayHiToGrandma(string){
-  if (string.toUpperCase()){
-    return "YES INDEED!"}
-  }
+
 
 function sayHiToGrandma(string){
   if (string === "I love you, Grandma."){
